8c96b4debde2a8ff52b7facfaa54ff76c19358c1
angharadcaswell <74596486+angharadcaswell@users.noreply.github.com>
2021-02-10 11:27:45 +0000

    Added Footer

---
+++ src/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles.css";
+
+export default function Footer() {
+  return (
+    <div className="Footer">
+      <p>
+        <a
+          href="https://github.com/angharadcaswell/shecodes-challenge"
+          target="_blank"
+        >
+          Open-source code{" "}
+        </a>
+        by Angharad Caswell
+      </p>
+    </div>
+  );
+}
